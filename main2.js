1d213bc31ed505a7dae947f4c456c628b8498a6f
mamma1234 <mamma1234@gmail.com>
2019-10-18 08:16:30 +0900

    first commit

--- main2.js
+++ main2.js
@@ -13,7 +13,6 @@ var app = http.createServer(function(request,response){
     }
     response.writeHead(200);
 	console.log(__dirname + _url);
-	console.log(__dirname + _url);
     response.end(fs.readFileSync(__dirname + _url));
 
 });
